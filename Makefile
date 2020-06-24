ca0f8d1d84bbcd254718ad42ef059c0195b95e46
sebastien <s.gelis@ksd.fr>
2020-06-24 13:46:25 +0200

    Upgrade to PEP517 build.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 clean:
-	rm -r package/build package/dist package/django_material_admin.egg-info
+	rm -r build dist django_material_admin.egg-info
 
 prepare:
 	npm install
@@ -8,7 +8,7 @@ prepare:
 
 build:
 	node_modules/gulp/bin/gulp.js
-	venv/bin/python package/setup.py sdist bdist_wheel
+	venv/bin/python -m pep517.build .
 
 scp:
-	scp package/dist/* pip@pip.ksd.fr:repo/django-material-admin/
+	scp dist/* pip@pip.ksd.fr:repo/django-material-admin/
---
+++ setup.py
@@ -0,0 +1,58 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+
+###################################################################
+
+NAME = "django-material-admin"
+PACKAGES = find_packages(exclude=("app", "app.*", "tests", "tests.*"))
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Framework :: Django :: 2.2",
+    "Framework :: Django :: 3.0",
+]
+INSTALL_REQUIRES = []
+
+###################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description="Material Design For Django Administration",
+        license="MIT License",
+        url="https://github.com/KSD-France/django-material-admin",
+        version="1.7.4",
+        author="KSD",
+        author_email="contact@ksd.fr",
+        maintainer="KSD",
+        maintainer_email="contact@ksd.fr",
+        long_description=read("README.md"),
+        long_description_content_type="text/markdown",
+        packages=PACKAGES,
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        options={"bdist_wheel": {"universal": "1"}},
+        include_package_data=True,
+    )

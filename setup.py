2a2cb8f55fd477944f1de63a3356c4ce0338f9b0
sebastien <s.gelis@ksd.fr>
2020-07-16 16:26:23 +0200

    Fix readonly fields display.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         description="Material Design For Django Administration",
         license="MIT License",
         url="https://github.com/KSD-France/django-material-admin",
-        version="1.3.3-ksd",
+        version="1.4.0-ksd",
         author="Anton Maistrenko",
         author_email="it2015maistrenko@gmail.com",
         maintainer="KSD",
